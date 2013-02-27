de743c890da8e5eeddac9e8a4976fa8958eb0114
Kaleb Murphy-amz <ec2-user@ip-10-242-189-86.ec2.internal>
2013-02-27 04:54:09 +0000

    Working on getting the center circle to work for inner radii of 0

--- public/RingGeometry.js
+++ public/RingGeometry.js
@@ -6,7 +6,7 @@ THREE.RingGeometry = function ( innerRadius, outerRadius, thetaSegments, phiSegm
 
     THREE.Geometry.call( this );
 
-    innerRadius = innerRadius || 25;
+    innerRadius = innerRadius || 0;
     outerRadius = outerRadius || 50;
 
     thetaStart = thetaStart !== undefined ? thetaStart : 0;
@@ -14,51 +14,74 @@ THREE.RingGeometry = function ( innerRadius, outerRadius, thetaSegments, phiSegm
 
     thetaSegments = thetaSegments !== undefined ? Math.max( 3, thetaSegments ) : 8;
     phiSegments = phiSegments !== undefined ? Math.max( 3, phiSegments ) : 8;
-
-    var i, o, uvs = [], radius = innerRadius, radiusStep = ( ( outerRadius - innerRadius ) / phiSegments), e = ( ( 1+ Math.sqrt(5) ) / 2 );
     
+    var i, o, uvs = [], radius = innerRadius, radiusStep = ( ( outerRadius - innerRadius ) / phiSegments);
+   
     for( i = 0; i <= phiSegments; i++) {//concentric circles inside ring
 
+        if( innerRadius === 0 && i === 0 ) {
+            var vertex = new THREE.Vector3();
+            this.vertices.push( vertex );
+            uvs.push( new THREE.Vector2( ( vertex.x / radius + 1 ) / 2, - ( vertex.y / radius + 1 ) / 2 + 1 ) );
+            continue;
+        }
+        
         for( o = 0; o <= thetaSegments; o++) {//number of segments per ring
 
             var vertex = new THREE.Vector3();
-
+            
             vertex.x = radius * Math.cos( thetaStart + o / thetaSegments * thetaLength );
             vertex.y = radius * Math.sin( thetaStart + o / thetaSegments * thetaLength );
-
+            
             this.vertices.push( vertex );
             uvs.push( new THREE.Vector2( ( vertex.x / radius + 1 ) / 2, - ( vertex.y / radius + 1 ) / 2 + 1 ) );
-
-            radius += radiusStep;
-            
         }
+        
+        radius += radiusStep;
 
-//        thetaSegments = thetaSegments * e;
     }
 
-    var n = new THREE.Vector3( 0, 0, -1 );
+    var n = new THREE.Vector3( 0, 0, 1 );
     
     for( i = 0; i < phiSegments; i++) {//concentric circles inside ring
 
         for( o = 0; o <= thetaSegments; o++) {//number of segments per ring
+            var v1, v2, v3;
 
-            var v1 = o;
-            var v2 = o + thetaSegments ;
-            var v3 = o + thetaSegments + 1 ;
+            if( innerRadius === 0 && i === 0 ) {
+                v1 = 0;
+                v2 = o+1;
+                v3 = o+2;
+                this.faces.push( new THREE.Face3( v1, v2, v3, [ n, n, n ] ) );
+                this.faceVertexUvs[ 0 ].push( [ uvs[ v1 ], uvs[ v2 ], uvs[ v3 ] ]);
+                continue;
+            }
 
+            v1 = o + (thetaSegments * i) + i;
+            v3 = o + (thetaSegments * i) + thetaSegments + i;
+            v2 = o + (thetaSegments * i) + thetaSegments + 1 + i;
+            
             this.faces.push( new THREE.Face3( v1, v2, v3, [ n, n, n ] ) );
             this.faceVertexUvs[ 0 ].push( [ uvs[ v1 ], uvs[ v2 ], uvs[ v3 ] ]);
             
-            v1 = o;
-            v2 = o + 1;
-            v3 = o + thetaSegments + 1 ;
-
+            v1 = o + (thetaSegments * i) + i;
+            v3 = o + (thetaSegments * i) + 1 + i;
+            v2 = o + (thetaSegments * i) + thetaSegments + 1 + i;
+            
             this.faces.push( new THREE.Face3( v1, v2, v3, [ n, n, n ] ) );
             this.faceVertexUvs[ 0 ].push( [ uvs[ v1 ], uvs[ v2 ], uvs[ v3 ] ]);
 
         }
     }
-   
+    
+/*    if(innerRadius === 0){
+        var circ = new THREE.CircleGeometry(innerRadius + radiusStep, thetaSegments, thetaStart, thetaLength);
+        this.vertices = this.vertices.concat(circ.vertices);
+        this.faces = this.faces.concat(circ.faces);
+        this.faceVertexUvs = this.faceVertexUvs.concat(circ.faceVertexUvs);
+    }*/
+//    this.mergeVertices();
+
     this.computeCentroids();
     this.computeFaceNormals();
 
--- public/main.js
+++ public/main.js
@@ -52,12 +52,20 @@ function start(){
 //    plane.position = new THREE.Vector3(0,0,0);
 //    scene.add(plane);
     //var circleGeo = new THREE.CircleGeometry(10, 5, Math.PI, Math.PI);
-    var circleGeo = new THREE.RingGeometry(5, 10, 10, 5, 0, Math.PI * 2);//10, 5, Math.PI, Math.PI);
-    var circle = new THREE.Mesh(circleGeo, new THREE.MeshPhongMaterial( { color: 0x555555, specular: 0xffaa00, shininess: 5, wireframe: true } )); 
-    console.dir(circleGeo);
-    scene.add(circle);
+    var thetas = 30, phis = 5, wf = true;
+    
+    var cgeo = new THREE.RingGeometry(5, 10, thetas, phis, 0, Math.PI * 2);//10, 5, Math.PI, Math.PI);
+    var c= new THREE.Mesh(cgeo, new THREE.MeshPhongMaterial( { color: 0xFFFF00, specular: 0xffaa00, shininess: 5, wireframe: wf } )); 
+    scene.add(c);
+    var cGeo2 = new THREE.RingGeometry(10, 15, thetas, phis, 0, Math.PI * 2);//10, 5, Math.PI, Math.PI);
+    var c2 = new THREE.Mesh(cGeo2, new THREE.MeshPhongMaterial( { color: 0xFF0000, specular: 0xffaa00, shininess: 5, wireframe: wf } )); 
+    scene.add(c2);
+    var cGeo3 = new THREE.RingGeometry(1, 5, thetas, phis, 0, Math.PI * 2);//10, 5, Math.PI, Math.PI);
+    var c3 = new THREE.Mesh(cGeo3, new THREE.MeshPhongMaterial( { color: 0x00FF00, specular: 0xffaa00, shininess: 5, wireframe: wf } )); 
+    scene.add(c3);
 
-    var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 ); 
+
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 ); 
     directionalLight.position.set( 1, 1, 1 ); 
     scene.add( directionalLight );
 
